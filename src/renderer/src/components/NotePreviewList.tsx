1b7df42f673d490d244edc785c7b9762c349ad19
Blank-code-bit <kelvincheah02@gmail.com>
2024-01-30 09:10:00 +0800

    markdown

---
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -0,0 +1,36 @@
+import { NotePreview } from '@/components'
+import { useNotesList } from '@/hooks/useNotesList'
+import { notesMock } from '@renderer/store/mocks'
+import { isEmpty } from 'lodash'
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export type NotePreviewListProps = ComponentProps<'ul'> & {
+  onSelect?: () => void
+}
+
+export const NotePreviewList = ({ onSelect, className, ...props }: NotePreviewListProps) => {
+  const { notes, selectedNoteIndex, handleNoteSelect } = useNotesList({ onSelect })
+
+  if (!notes) return null
+
+  if (isEmpty(notes)) {
+    return (
+      <ul className={twMerge('text-center pt-4', className)} {...props}>
+        <span>No Notes Yet@</span>
+      </ul>
+    )
+  }
+  return (
+    <ul className={className} {...props}>
+      {notesMock.map((note, index) => (
+        <NotePreview
+          key={note.title + note.lastEditTime}
+          isActive={selectedNoteIndex === index}
+          onClick={handleNoteSelect(index)}
+          {...note}
+        />
+      ))}
+    </ul>
+  )
+}
